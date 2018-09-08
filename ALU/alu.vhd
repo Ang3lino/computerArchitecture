13fd39db23fed3e8344f95efae0b7c4a54095009
Angel Lopez Manriquez <sigma271@outlook.com>
2018-09-07 23:59:55 -0500

    compila, parece que hay errores con la resta

--- ALU/alu.vhd
+++ ALU/alu.vhd
@@ -1,36 +1,52 @@
 library ieee;
 
 use ieee.std_logic_1164.all;
-use work.arith.all; -- include a package
-
+use work.arith.all; -- include all the components from a personal package
 
 -- Author: Angel Lopez Manriquez
 -- FPGA model: cyclone ll, EP2C5T144C8N
 
 entity alu is 
     generic ( 
-        n: integer := 4 -- we'll use four bits
+        n: integer := 4 -- amount of bits to use
     );
     port (
         a, b: in std_logic_vector (n - 1 downto 0);
-        b_invert: in std_logic; -- 0 for sum, 1 for subtract
-		  c: inout std_logic_vector(n downto 0);
-        cf: out std_logic; 
-        s: out std_logic_vector (n - 1 downto 0)
+        aluop: in std_logic_vector(3 downto 0);
+        flags: out std_logic_vector(3 downto 0); 
+        ans: out std_logic_vector (n - 1 downto 0)
     );
 end entity;
 
 architecture angel of alu is
-
-
+    signal gnd: std_logic := '0';
+	signal c: std_logic_vector(n downto 0);
 begin
 
-    process(a, b, b_invert) is 
+    -- aluop definitions
+    -- aluop(3): a_invert
+    -- aluop(2): b_invert
+    -- aluop(1): mux(1)
+    -- aluop(0): mux(0)
+    process(a, b, aluop) is 
+        variable a_invert: std_logic := aluop(3);
+        variable b_invert: std_logic := aluop(2);
+
+        variable muxa: std_logic_vector(n - 1 downto 0) ;
+        variable muxb: std_logic_vector(n - 1 downto 0) ;
     begin
-        c <= carry_with_sum(a, b, b_invert);
-        for i in 0 to n - 1 loop 
-            s(i) <= c(i) xor a(i) xor b(i) xor b_invert;
+        for i in n - 1 downto 0 loop
+            muxa(i) := a_invert xor a(i);
+            muxb(i) := b_invert xor b(i);
         end loop;
+        case (aluop(1 downto 0)) is 
+            when "00" => ans <= muxa and muxb;
+            when "01" => ans <= muxa or muxb;
+            when "10" => ans <= muxa xor muxb;
+            when others =>
+                c <= carries_of_sum(muxa, muxb, b_invert);
+                ans <= c(n - 1 downto 0) xor muxa xor muxb;
+        end case;
     end process;
 
 end architecture ;
--- ALU/arith.vhd
+++ ALU/arith.vhd
@@ -4,21 +4,43 @@ use ieee.std_logic_1164.all;
 
 package arith is
 
-    function carry_with_sum(
+    function carries_of_sum(
         a: std_logic_vector;
         b: std_logic_vector;
         b_invert: std_logic ) return std_logic_vector;
 
     function vprod (vec: std_logic_vector) return std_logic;
 
+    function vxor_pair(u: std_logic_vector; v: std_logic_vector) 
+        return std_logic_vector;
+
 end package ; -- own 
 
 package body arith is 
 
+    function vxor_pair(u: std_logic_vector; v: std_logic_vector) 
+    return std_logic_vector is 
+        variable solution: std_logic_vector(u'length - 1 downto 0);
+    begin 
+        for i in u'length - 1 downto 0 loop
+            solution(i) := u(i) xor v(i);
+        end loop;
+    end function;
+
+	function vprod (vec: std_logic_vector) 
+	return std_logic is
+		variable result : std_logic := '1';
+	begin 
+		for i in vec'length - 1 downto 0 loop 
+			result := result and vec(i);
+		end loop;
+		return result;
+	end function;
+
     -- if b_invert is 0 we add else we subtract.
     -- the lenght of a must be equal to b.
     -- if a has n bits c must have n + 1 bits
-    function carry_with_sum(a: std_logic_vector;
+    function carries_of_sum(a: std_logic_vector;
                             b: std_logic_vector;
                             b_invert: std_logic ) 
     return std_logic_vector is 
@@ -47,14 +69,5 @@ package body arith is
     end function;
 
 
-	function vprod (vec: std_logic_vector) 
-	return std_logic is
-		variable result : std_logic := '1';
-	begin 
-		for i in vec'length - 1 downto 0 loop 
-			result := result and vec(i);
-		end loop;
-		return result;
-	end function;
 
 end package body;
