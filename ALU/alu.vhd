3cc50330b5d5b2f1157c434f1db9e8490b1c23bc
Angel Lopez Manriquez <sigma271@outlook.com>
2018-09-08 17:22:12 -0500

    Todo parece funcionar
    Nota: -a-b, a pesar de ser necesario, tiene errores pues se hace -a-b+1

--- ALU/alu.vhd
+++ ALU/alu.vhd
@@ -1,14 +1,18 @@
 library ieee;
 
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all; -- to_unsigned
+
 use work.arith.all; -- include all the components from a personal package
 
 -- Author: Angel Lopez Manriquez
 -- FPGA model: cyclone ll, EP2C5T144C8N
 
+-- we don't define -a - b yet.
+
 entity alu is 
     generic ( 
-        n: integer := 4 -- amount of bits to use
+        n: integer := 128 -- amount of bits to use
     );
     port (
         a, b: in std_logic_vector (n - 1 downto 0);
@@ -19,9 +23,27 @@ entity alu is
 end entity;
 
 architecture angel of alu is
-    signal gnd: std_logic := '0';
-	signal c: std_logic_vector(n downto 0);
+   --signal gnd: std_logic := '0';
+    signal c: std_logic_vector(n downto 0); -- carries
+    signal s_ans: std_logic_vector(n - 1 downto 0);
+    
+    -- very useful constant, a "zero vector". It was needed for the when else instruction
+    constant zero: std_logic_vector(n - 1 downto 0) := (others => '0');
+
 begin
+
+    -- flags definitions
+    -- overflow := flags(3);
+    -- negative := flags(2);
+    -- zero := flags(1);
+    -- carry := flags(0);
+
+    ans <= s_ans;
+
+    flags(3) <= c(n) xor c(n - 1); -- Overflow
+    flags(2) <= s_ans(n - 1); -- Negative
+    flags(1) <= '1' when s_ans = zero else '0'; -- Zero
+    flags(0) <= c(n); -- Final Carry
 
     -- aluop definitions
     -- aluop(3): a_invert
@@ -40,12 +62,12 @@ begin
             muxb(i) := b_invert xor b(i);
         end loop;
         case (aluop(1 downto 0)) is 
-            when "00" => ans <= muxa and muxb;
-            when "01" => ans <= muxa or muxb;
-            when "10" => ans <= muxa xor muxb;
+            when "00" => s_ans <= muxa and muxb;
+            when "01" => s_ans <= muxa or muxb;
+            when "10" => s_ans <= muxa xor muxb;
             when others =>
-                c <= carries_of_sum(muxa, muxb, b_invert);
-                ans <= c(n - 1 downto 0) xor muxa xor muxb;
+                c <= carries_of_sum(muxa, b, b_invert); 
+                s_ans <= c(n - 1 downto 0) xor muxa xor muxb;
         end case;
     end process;
 
